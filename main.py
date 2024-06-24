73b79192c1cce9fd5686848120474a43c1520965
Sergio Junior <sergio.junior@stormgroup.com.br>
2024-06-23 22:04:26 -0300

    created a folder to the deployments and creates a script in the main file to run the prefect commands

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+import subprocess
+import time
+
 if __name__ == "__main__":
-    print('hellooo')
+    print('Running the script:::')
+    server_process = subprocess.Popen(["prefect", "server", "start"])
+    time.sleep(10)
+    deployment_process = subprocess.run(["python3",
+                                         "deployments/rick_morty_deployments"
+                                         ".py"])
+    server_process.wait()
+
